f641bcfb884f9e4b7acc170c431146c93ae0cf00
Pabitra-1 <Pabitrapal49@gmail.com>
2026-06-27 21:02:52 +0530

    Add string comparison and concatenation scripts

---
+++ string/compare.py
@@ -0,0 +1,7 @@
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+if str1 == str2:
+    print("Strings are same")
+else:
+    print("Strings are different")
---
+++ string/concatinate.py
@@ -0,0 +1,7 @@
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+# Concatenating two strings
+ans = str1 + str2
+
+print("Concatenated string is:", ans)
