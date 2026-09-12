b7541658ad9055fd25c3a3e9a72278d0b4e2d583
Pabitra-1 <Pabitrapal49@gmail.com>
2026-09-12 18:52:05 +0530

    Update randomModule.py to use randrange instead of randint; add statisticModule.py for statistical calculations

--- Ass9/randomModule.py
+++ Ass9/randomModule.py
@@ -1,4 +1,4 @@
 import random as rd
 list = [1,2,3,4,5,6,7,8,9]
 print(rd.choice(list))
-print(rd.randint(0,100))
+print(rd.randrange(0,100))
---
+++ Ass9/statisticModule.py
@@ -0,0 +1,8 @@
+import statistics as st
+data = [4,8,5,2,88,2,9,7]
+print("Mean is : ", st.mean(data))
+print("Median is : ",st.median(data))
+print("Mode is : ",st.mode(data))
+print("Standard deviation is : ", st.stdev(data))
+print("High median is : ", st.median_high(data))
+print("Low median is : ", st.median_low(data))
