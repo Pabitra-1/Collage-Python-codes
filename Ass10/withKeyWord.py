4ec953be139783f26ca764b2a6284ef143dc663f
Pabitra-1 <Pabitrapal49@gmail.com>
2026-09-12 18:59:38 +0530

    Add withKeyWord.py and withoutKeyWord.py for student function demonstration

---
+++ Ass10/withKeyWord.py
@@ -0,0 +1,4 @@
+def student(name, age, roll_no):
+    print(f"Name : {name} , Age : {age} , Roll No : {roll_no}")
+
+student(name="Rengoku", age=25, roll_no=123)
---
+++ Ass10/withoutKeyWord.py
@@ -0,0 +1,3 @@
+def student(name, age, roll_no):
+    print(f"Name : {name} , Age : {age} , Roll No : {roll_no}")
+student("Rengoku", 25, 123)
